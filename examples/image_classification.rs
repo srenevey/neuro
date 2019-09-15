626d2729ee79dcb33cb572eec1ff7ca427a92f2f
srenevey <syl.renevey@gmail.com>
2019-09-15 14:45:07 -0400

    Misc

--- examples/image_classification.rs
+++ examples/image_classification.rs
@@ -1,23 +1,21 @@
 use std::path::Path;
-use arrayfire::*;
 
 use neuro::data::{DataSetError, ImageDataSet};
 use neuro::models::Network;
 use neuro::losses::Loss;
 use neuro::activations::Activation;
 use neuro::optimizers::Adam;
-use neuro::layers::{Dense, Conv2D};
+use neuro::layers::{Dense, Conv2D, ConvMode};
 
 
 fn main() -> Result<(), DataSetError> {
 
     let path = Path::new("datasets/MNIST");
-    let data = ImageDataSet::from_path(&path, 28)?;
+    let mut data = ImageDataSet::from_path(&path, 28)?;
     data.print_stats();
 
-    //let mut nn = Network::new(&data, Loss::CrossEntropy, Adam::new(0.03));
-    //nn.add(Conv2D::new(Activation::ReLU, 3, ConvMode::DEFAULT));
-    //nn.add(Dense::new(2, Activation::Softmax));
+    let mut nn = Network::new(&mut data, Loss::CrossEntropy, Adam::new(0.03));
+    nn.add(Conv2D::new(2, (32, 32), ConvMode::Same, Activation::Linear));
 
     Ok(())
 }
