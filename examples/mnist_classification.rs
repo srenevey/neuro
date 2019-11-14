282165453ffcf1d0a828374123616c9cf0c1812a
srenevey <syl.renevey@gmail.com>
2019-11-13 21:56:17 -0500

    Initial commit

--- examples/mnist_classification.rs
+++ examples/mnist_classification.rs
@@ -13,11 +13,10 @@ use neuro::tensor::*;
 
 fn main() -> Result<(), DataSetError> {
 
+    // Create the dataset
     let path = Path::new("datasets/MNIST");
     let data = ImageDataSet::from_path(&path, 28, 0.2)?;
     println!("{}", data);
-    data.print_classes();
-
 
     let mut nn = Network::new(&data, losses::SoftmaxCrossEntropy, Adam::new(0.03), Some(vec![Metrics::Accuracy]), None);
     nn.add(Dense::new(32, Activation::ReLU));
@@ -25,32 +24,28 @@ fn main() -> Result<(), DataSetError> {
 
     println!("{}", nn);
 
-    nn.fit(128, 50, Some(1));
+    nn.fit(128, 2, Some(1));
 
 
     // Predict output on test images
-    let img1 = data.load_img(Path::new("datasets/MNIST/test/2/img_1.jpg"))?;
-    let img2 = data.load_img(Path::new("datasets/MNIST/test/1/img_18.jpg"))?;
-    let img3 = data.load_img(Path::new("datasets/MNIST/test/0/img_4.jpg"))?;
-    let img4 = data.load_img(Path::new("datasets/MNIST/test/3/img_10.jpg"))?;
+    let input = data.load_img_vec(&vec![
+        Path::new("datasets/MNIST/test/2/img_1.jpg"),
+        Path::new("datasets/MNIST/test/1/img_18.jpg"),
+        Path::new("datasets/MNIST/test/0/img_9.jpg"),
+        Path::new("datasets/MNIST/test/3/img_10.jpg")
+    ])?;
 
-    let pred1 = nn.predict(&img1);
-    let pred2 = nn.predict(&img2);
-    let pred3 = nn.predict(&img3);
-    let pred4 = nn.predict(&img4);
-
-    println!("prediction for '2':");
-    pred1.print_tensor();
-
-    println!("prediction for '1':");
-    pred2.print_tensor();
-
-    println!("prediction for '0':");
-    pred3.print_tensor();
-
-    println!("prediction for '3':");
-    pred4.print_tensor();
-
+    let predictions = nn.predict_class(&input);
+    print_prediction(&predictions);
 
     Ok(())
+}
+
+fn print_prediction(predictions: &Vec<(String, PrimitiveType)>) {
+    println!("Predictions:");
+    let mut index = 0;
+    for (class, probability) in predictions {
+        index += 1;
+        println!("image {}: class: {}, probability: {}", index, class, probability);
+    }
 }
---
+++ src/data/batch_iterator.rs
@@ -0,0 +1,71 @@
+use crate::tensor::*;
+use arrayfire::*;
+
+pub(crate) struct BatchIterator<'a> {
+    data: (&'a Tensor, &'a Tensor),
+    num_samples: u64,
+    batch_size: u64,
+    batch: u64,
+    num_batches: u64
+}
+
+impl<'a> BatchIterator<'a> {
+
+    /// Creates a batch iterator of given size for the two Tensors.
+    ///
+    /// # Arguments
+    /// * `data`: tuple of reference on the Tensors
+    /// * `batch_size`: size of the mini-batches
+    ///
+    pub(crate) fn new(data: (&'a Tensor, &'a Tensor), batch_size: u64) -> BatchIterator<'a> {
+        // Check that both tensors have the same number of samples
+        assert_eq!(data.0.dims().get()[3], data.1.dims().get()[3]);
+        let num_samples = data.0.dims().get()[3];
+
+        let (batch_size, num_batches) = if batch_size < num_samples {
+            let num_batches = (num_samples as f64 / batch_size as f64).ceil() as u64;
+            (batch_size, num_batches)
+        } else {
+            (num_samples, 1)
+        };
+
+        BatchIterator {
+            data,
+            num_samples,
+            batch_size,
+            batch: 0,
+            num_batches
+        }
+    }
+
+    /// Returns the number of batches that the iterator will produce.
+    pub(crate) fn num_batches(&self) -> u64 {
+        self.num_batches
+    }
+}
+
+impl<'a> std::iter::Iterator for BatchIterator<'a> {
+    type Item = (Tensor, Tensor);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.batch < self.num_batches {
+            // Compute lower and upper bounds to retrieve samples
+            let lb = (self.batch * self.batch_size) as usize;
+            let mut ub = ((self.batch + 1) * self.batch_size - 1) as usize;
+            if ub >= self.num_samples as usize {
+                ub = (self.num_samples - 1) as usize;
+            }
+
+            // Create mini-batches
+            let seqs = &[Seq::default(), Seq::default(), Seq::default(), Seq::new(lb as f64, ub as f64, 1.0)];
+            let mini_batch_x = index(&self.data.0, seqs);
+            let mini_batch_y = index(&self.data.1, seqs);
+
+            self.batch += 1;
+
+            Some((mini_batch_x, mini_batch_y))
+        } else {
+            None
+        }
+    }
+}
