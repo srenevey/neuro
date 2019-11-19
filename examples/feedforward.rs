30fb055e688c523d03bd7dcd337234aa07ff7f8a
srenevey <syl.renevey@gmail.com>
2019-11-18 20:24:16 -0500

    Changed initializers. Remove arrayfire dependencies

--- examples/feedforward.rs
+++ examples/feedforward.rs
@@ -1,39 +1,37 @@
-use std::path::Path;
-
-use neuro::layers::{Dense, BatchNormalization};
+use neuro::layers::Dense;
 use neuro::activations::Activation;
 use neuro::models::Network;
-use neuro::losses::Loss;
+use neuro::losses;
 use neuro::optimizers::Adam;
 use neuro::data::{DataSetError, TabularDataSet};
+use neuro::tensor::*;
+
+use std::path::Path;
 
 
 fn main() -> Result<(), DataSetError> {
 
-    let inputs = Path::new("datasets/inputs.csv");
-    let outputs = Path::new("datasets/outputs.csv");
-
-    let mut data = TabularDataSet::from_csv(&inputs, &outputs, 0.2)?;
-    data.normalize_output();
+    // Load the data
+    let inputs = Path::new("datasets/tabular_data/input_normalized.csv");
+    let outputs = Path::new("datasets/tabular_data/output_normalized.csv");
+    let data = TabularDataSet::from_csv(&inputs, &outputs, 0.1, true)?;
+    println!("{}", data);
 
     // Create the network
-    let mut nn = Network::new(&mut data, Loss::MeanSquaredError, Adam::new(0.03));
-    nn.add(BatchNormalization::new());
-    nn.add(Dense::new(4, Activation::ReLU));
-    nn.add(BatchNormalization::new());
-    nn.add(Dense::new(5, Activation::ReLU));
-    nn.add(BatchNormalization::new());
+    let mut nn = Network::new(&data, losses::MeanSquaredError, Adam::new(0.01), None, None);
+    nn.add(Dense::new(32, Activation::ReLU));
+    nn.add(Dense::new(15, Activation::ReLU));
     nn.add(Dense::new(2, Activation::Linear));
 
     // Train
-    nn.fit(32, 100);
+    nn.fit(64, 50, Some(10));
 
-    // Test
-    nn.evaluate();
-
+    // Predictions: create two inputs: (-0.5, 0.92, 0.35) and (0.45, -0.72, -0.12).
+    let inputs = Tensor::new(&[-0.5, 0.92, 0.35, 0.45, -0.72, -0.12], Dim::new(&[3, 1, 1, 2]));
+    let res = nn.predict(&inputs); // expected: (-0.957, 1.1314) and (-0.4644, 0.7884) respectively.
+    println!("Predictions:");
+    res.print_tensor();
 
     Ok(())
 }
-
-
 
