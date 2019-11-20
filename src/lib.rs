f5585f3e44898fbcdb71d7c7495a0ce561cdccb7
srenevey <syl.renevey@gmail.com>
2019-11-19 22:47:41 -0500

    Added comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,44 @@
 //! # Neuro
 //!
-//! A library to do deep learning on the GPU. The architecture of the library is inspired by Keras and works by stacking layers.
+//! Neuro is a deep learning library that runs on the GPU. The architecture of the library is inspired by Keras and works by stacking layers.
 //! Currently supported layers are:
 //! * BatchNorm
-//! * Conv2D
+//! * Conv2D (currently broken, see note 2 below)
 //! * Dense
 //! * Dropout
 //! * MaxPooling2D
 //!
 //! allowing the creation of feedforward and convolutional neural networks.
+//!
+//! ### Note 1
+//! The crate is under heavy development and several features have not yet been implemented. Among them
+//! is the ability to save a trained network.
+//!
+//! ### Note 2
+//! When creating convolutional neural networks, there is a bug when trying to retrieve the
+//! values from the GPU (the process freezes). This means that the loss and metrics cannot be printed
+//! and therefore there is no way to assess if the network is actually learning something.
+//!
+//! ## Installation
+//! The crate is powered by ArrayFire to perform all operations on the GPU. The first step is therefore to
+//! [install ArrayFire](https://crates.io/crates/arrayfire). Once the library is installed, clone this repository
+//! and start using neuro by importing it in your project:
+//! ```
+//! use neuro::*;
+//! ```
+//! When building your project, make sure that the path to the ArrayFire library is in the path environment
+//! variables. For instance for a typical install (on Unix):
+//! ```
+//! export DYLD_LIBRARY_PATH=/opt/arrayfire/lib
+//! ```
+//! In order to quickly get started, check out the [examples](https://srenevey.github.io/neuro).
+
 
 pub use self::tensor::Tensor;
 
 pub mod activations;
 pub mod data;
+pub mod initializers;
 pub mod layers;
 pub mod losses;
 pub mod metrics;
