8a9a4fbd46549b43e01c1ec3ed4b27bfe3d55033
srenevey <syl.renevey@gmail.com>
2019-11-19 22:26:08 -0500

    Currently broken

--- examples/image_classification_cnn.rs
+++ examples/image_classification_cnn.rs
@@ -1,41 +1,34 @@
+// CURRENTLY BROKEN
+use neuro::activations::Activation;
+use neuro::data::{DataSetError, ImageDataSet};
+use neuro::layers::{Dense, Conv2D, Padding, MaxPooling2D};
+use neuro::losses;
+use neuro::metrics::Metrics;
+use neuro::models::Network;
+use neuro::optimizers::SGD;
+
 use std::path::Path;
 
-use neuro::data::{DataSetError, ImageDataSet, DataSet};
-use neuro::models::Network;
-use neuro::losses::Loss;
-use neuro::activations::Activation;
-use neuro::optimizers::Adam;
-use neuro::layers::{Dense, Conv2D, Padding, MaxPooling2D};
-
-use arrayfire::*;
 
 fn main() -> Result<(), DataSetError> {
 
+    // Load the data
     let path = Path::new("datasets/MNIST");
-    let mut data = ImageDataSet::from_path(&path, 28)?;
-    data.print_stats();
+    let data = ImageDataSet::from_path(&path, 28, 0.01)?;
+    println!("{}", data);
 
-    let test = randu::<f64>(Dim4::new(&[3, 10, 1, 1]));
-    af_print!("test", test);
-    let tmp = gt(&test, &0.7, true);
-    let res = mul(&test, &tmp, true);
-    af_print!("res", res);
-
-    println!("passed");
-    //let test2 = randn::<f64>(Dim4::new(&[10976000, 1, 1, 1]));
-
-
-
-    /*
-    let mut nn = Network::new(&mut data, Loss::CrossEntropy, Adam::new(0.001));
-    nn.add(Conv2D::new(28, (3, 3), (1, 1), Padding::Same));
-    nn.add(MaxPooling2D::new((2, 2)));
-    nn.add(Dense::new(128, Activation::ReLU));
+    // Create the neural network
+    let mut nn = Network::new(&data, losses::SoftmaxCrossEntropy, SGD::new(0.001), Some(vec![Metrics::Accuracy]), None);
+    nn.add(Conv2D::new(32, (5, 5), (1, 1), Padding::Same));
+    nn.add(Conv2D::new(64, (3, 3), (1, 1), Padding::Same));
+    nn.add(MaxPooling2D::new((2, 2), (2, 2)));
+    nn.add(Dense::new(32, Activation::ReLU));
     nn.add(Dense::new(10, Activation::Softmax));
 
-    nn.fit(32, 10);
-*/
+    println!("{}", nn);
 
+    // Fit the model
+    nn.fit(32, 5, Some(1));
 
     Ok(())
 }
