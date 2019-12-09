d442b37b3a6552765ce9cf65f6f39a0ddd96186c
srenevey <syl.renevey@gmail.com>
2019-12-08 21:47:16 -0500

    Fixed bug that prevented CNN to work

--- examples/image_classification_cnn.rs
+++ examples/image_classification_cnn.rs
@@ -1,34 +1,39 @@
-// CURRENTLY BROKEN
 use neuro::activations::Activation;
-use neuro::data::{DataSetError, ImageDataSet};
-use neuro::layers::{Dense, Conv2D, Padding, MaxPooling2D};
+use neuro::data::ImageDataSet;
+use neuro::errors::*;
+use neuro::layers::{Dense, Conv2D, Padding, MaxPooling2D, Dropout};
 use neuro::losses;
 use neuro::metrics::Metrics;
 use neuro::models::Network;
-use neuro::optimizers::SGD;
+use neuro::optimizers::Adam;
+use neuro::regularizers::*;
 
 use std::path::Path;
 
-
-fn main() -> Result<(), DataSetError> {
+fn main() -> Result<(), NeuroError> {
 
     // Load the data
     let path = Path::new("datasets/MNIST");
-    let data = ImageDataSet::from_path(&path, 28, 0.01)?;
+    let data = ImageDataSet::from_path(&path, 28, 0.1)?;
     println!("{}", data);
 
     // Create the neural network
-    let mut nn = Network::new(&data, losses::SoftmaxCrossEntropy, SGD::new(0.001), Some(vec![Metrics::Accuracy]), None);
-    nn.add(Conv2D::new(32, (5, 5), (1, 1), Padding::Same));
+    let mut nn = Network::new(&data, losses::SoftmaxCrossEntropy, Adam::new(0.001), None);
+    nn.add(Conv2D::new(32, (3, 3), (1, 1), Padding::Same));
     nn.add(Conv2D::new(64, (3, 3), (1, 1), Padding::Same));
     nn.add(MaxPooling2D::new((2, 2), (2, 2)));
-    nn.add(Dense::new(32, Activation::ReLU));
+    nn.add(Dropout::new(0.5));
+    nn.add(Dense::new(128, Activation::ReLU));
+    nn.add(Dropout::new(0.25));
     nn.add(Dense::new(10, Activation::Softmax));
 
     println!("{}", nn);
 
     // Fit the model
-    nn.fit(32, 5, Some(1));
+    nn.fit(128, 10, Some(1), Some(vec![Metrics::Accuracy]));
+
+    // Evaluate the trained model on the test set
+    nn.evaluate(Some(vec![Metrics::Accuracy]));
 
     Ok(())
 }
