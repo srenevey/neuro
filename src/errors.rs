558ff3fa8053c363741b12cc7298ded602d6f7f8
srenevey <syl.renevey@gmail.com>
2019-12-08 21:46:16 -0500

    Initial commit

---
+++ src/errors.rs
@@ -0,0 +1,23 @@
+use crate::data;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum NeuroError {
+    DataSetError(data::DataSetError),
+    NoLayer,
+}
+
+impl fmt::Display for NeuroError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            NeuroError::DataSetError(ref err) => write!(f, "DataSetError: {}", err),
+            NeuroError::NoLayer => write!(f, "The network doesn't contain any layer."),
+        }
+    }
+}
+
+impl std::convert::From<data::DataSetError> for NeuroError {
+    fn from(error: data::DataSetError) -> NeuroError {
+        NeuroError::DataSetError(error)
+    }
+}
